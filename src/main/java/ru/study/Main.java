5201815a5cbd1713628a68e15a1ceb73b743dfde
AnHi4 <an.hi.a.d.1120@gmail.com>
2024-04-25 01:33:21 +0300

    Employee Management System without Spring framework

--- src/main/java/ru/study/Main.java
+++ src/main/java/ru/study/Main.java
@@ -1,18 +1,13 @@
 package ru.study;
 
 import ru.study.controller.EmployeeManagementController;
-import ru.study.model.param.ChangeEmployeeParam;
 import ru.study.model.param.CreateEmployeeParam;
-import ru.study.repository.EmployeeRepository;
-import ru.study.service.EmployeeService;
-import ru.study.trash.Utils;
 
 import java.math.BigDecimal;
-import java.sql.*;
 import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) {
         final EmployeeManagementController employeeManagementController = new EmployeeManagementController();
 
         CreateEmployeeParam employeeBob = new CreateEmployeeParam("Bob", "Fetcher", "Johnson", "Male",
@@ -22,22 +17,21 @@ public class Main {
         CreateEmployeeParam employeeLena = new CreateEmployeeParam("lena", "Poleno", "xzkto", "Female",
                 "e3@mail", BigDecimal.valueOf(20000), "RUB", LocalDate.of(1999, 4, 2), LocalDate.now());
 
-//        employeeManagementController.createEmployee(employeeBob);
-//        employeeManagementController.createEmployee(employeeJohn);
-//        employeeManagementController.createEmployee(employeeLena);
-//
-//        System.out.println(employeeManagementController.getEmployees());
+        employeeManagementController.createEmployee(employeeBob);
+        employeeManagementController.createEmployee(employeeJohn);
+        employeeManagementController.createEmployee(employeeLena);
+
+        System.out.println(employeeManagementController.getEmployees());
         System.out.println(employeeManagementController.getChangeLogs());
         System.out.println(employeeManagementController.getChangeLogsByEmail("e2@mail"));
-//        System.out.println();
-//
-//        ChangeEmployeeParam employeeSalaryParam = new ChangeEmployeeParam(54, BigDecimal.valueOf(2000), "USD", "good worker");
-//
-//        employeeManagementController.editEmployeeSalary(employeeSalaryParam);
-//        System.out.println(employeeManagementController.getChangeLogs());
+        System.out.println();
+
+        //ChangeEmployeeParam employeeSalaryParam = new ChangeEmployeeParam(54, BigDecimal.valueOf(2000), "USD", "good worker");
+        //employeeManagementController.editEmployeeSalary(employeeSalaryParam);
+        System.out.println(employeeManagementController.getChangeLogs());
         System.out.println(employeeManagementController.getChangeLogsById(54));
-//
-//        System.out.println(employeeManagementController.findEmployeeById(58));
-//        System.out.println(employeeManagementController.findEmployeeByEmail("e1@mail"));
+
+        System.out.println(employeeManagementController.findEmployeeById(58));
+        System.out.println(employeeManagementController.findEmployeeByEmail("e1@mail"));
     }
 }
